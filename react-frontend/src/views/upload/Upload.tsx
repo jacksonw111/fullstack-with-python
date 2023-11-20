063e24e66c8fbd18639220bb48f6ca23a3524f50
jackson.wen <jackson.wen@xm.wonder.com>
2023-11-20 10:18:07 +0800

    adjust upload

--- react-frontend/src/views/upload/Upload.tsx
+++ react-frontend/src/views/upload/Upload.tsx
@@ -1,14 +1,17 @@
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export const Upload = () => {
   return (
     <div>
-      <p>
-        需要实现的功能，大文件分片上传。
-      </p>
-      <div>
-        <Input type="file" className="cursor-pointer"/>
+      <p>需要实现的功能，大文件分片上传。</p>
+      <div className="relative overflow-hidden w-64 cursor-pointer">
+        <Input
+          type="file"
+          className="cursor-pointer absolute top-0 left-0 opacity-0 w-full h-full"
+        />
+        <Button className="cursor-pointer">上传文件</Button>
       </div>
     </div>
-  )
-}
+  );
+};
